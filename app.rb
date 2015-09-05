f29e659bfcf33fa1189f906bbbdcce2e93aa4e4d
Sam Phippen <samphippen@googlemail.com>
2015-09-05 16:22:14 +0100

    Minimum viable service

--- app.rb
+++ app.rb
@@ -2,12 +2,22 @@ require "sinatra"
 require "json"
 
 require 'sinatra/cross_origin'
+require 'erb'
+require 'ostruct'
 
 configure do
   enable :cross_origin
 end
 
-$received = []
+
+class ErbalT < OpenStruct
+  def render(template)
+    ERB.new(template).result(binding)
+  end
+end
+
+
+$received = Hash.new { |h,k| h[k] = [] }
 
 before do
   if request.body.length > 0
@@ -16,16 +26,35 @@ before do
   end
 end
 
-options "/" do
-  ""
+def read_file(file_name)
+  File.open(File.join(File.dirname(File.absolute_path(__FILE__)), "files", file_name)).read
 end
 
-post "/" do
-  $received << @request_payload
-  ""
+def render_loader_javascript(user_id)
+  ErbalT.new(:user_id => user_id).render(read_file("loader.js.erb"))
+end
+
+
+get "/script_for/:user_id" do |user_id|
+  content_type "text/javascript"
+  ErbalT.new(:user_id => user_id).render(read_file("scraper.js.erb"))
 end
 
 get "/" do
+  user_id = params.fetch("user_id", "")
+  erb(:index, :locals => {:user_id => params.fetch("user_id", ""), :loader_javascript => "javascript:#{render_loader_javascript(user_id).gsub("\n", "")}"})
+end
+
+options "/*" do
+  ""
+end
+
+post "/listens/:user_id" do |user_id|
+  $received[user_id] << @request_payload
+  ""
+end
+
+get "/listens/:user_id" do |user_id|
   content_type "application/json"
-  JSON.dump($received)
+  JSON.dump($received[user_id])
 end
